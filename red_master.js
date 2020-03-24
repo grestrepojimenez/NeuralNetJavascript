90fcff9b6c1860018aedfd3f8df4b5c05617202e
Juan Giovanni <grestrepojimenez@gmail.com>
2020-03-24 10:14:13 -0500

    forwardPropagation

--- red_master.js
+++ red_master.js
@@ -1,20 +1,17 @@
 
-let settings = asignar_valores()
 
+let settings = assignValues()
 
-let neuralNet = new Array;
+let neuralNet = createNet(settings)
 
-for (i=0; i<settings.netStruct.length-1; i++){ 
-		neuralNet[i] = {
-		"sesgo" : matrix(settings.netStruct[i], 1).fill(1),
-		"pesos_sinapticos": matrix(settings.netStruct[i], settings.netStruct[i+1]),
-		"errors": matrix(settings.netStruct[i+1], settings.netStruct[i+2]),
-	}
-}
-	for (i=0; i<settings.netStruct.length-1; i++){ 
- 		// neuralNet[i].pesos_sinapticos[settings.netStruct[i]].map( ()=> Math.round(Math.random())); 
- 		// neuralNet[i].errors = matrix(settings.netStruct[i], settings.netStruct[i]+1 ).fill(1)
- 	}
+// while (settings.error >0.01 && settings.iterations<settings.maxIterations){
+	// for(j=0; j< settings.trainingInput.length; j++){
+		let j = 0
+		neuralNet = forwardPropagation(settings, neuralNet, j)
+		// neuralNet = backpropagtion(settings, neuralNet, j)
+		// neuralNet = synapticWeightsRecalc(settings, neuralNet)
+	// }
+// }
 
 
 
@@ -23,20 +20,15 @@ for (i=0; i<settings.netStruct.length-1; i++){
 
 
 
-
-
-
-function asignar_valores(){
+function assignValues(){
 	let	settings = {
 		"error" : 1,
-		"epocas" : 0,
-		"max_epocas":  10000,
-		"capas_ocultas" : 2,
-		"salida" : [0, 1, 1, 0, 1, 0, 0, 1],
-		"patron" : [[0, 0, 0],[0, 0, 1],[0, 1, 0],[0, 1, 1],[1, 0, 0],[1, 0, 1],[1, 1, 0],[1, 1, 1]],
-		"neuronas_1_capa" : 30,
-		"neuronas_2_capa": 20,
-		"netStruct" : [3, 30, 20, 1] //entredas, capa 1, capa2, salidas
+		"iterations" : 0,
+		"maxIterations":  10000,
+		"hiddenLayers" : 2,
+		"trainingOutput" : [0, 1, 1, 0, 1, 0, 0, 1],
+		"trainingInput" : [[0, 0, 0],[0, 0, 1],[0, 1, 0],[0, 1, 1],[1, 0, 0],[1, 0, 1],[1, 1, 0],[1, 1, 1]],
+		"netStruct" : [3, 30, 20, 1] //number of inputs, neurons 1 layer, neurons 2 layer, outputs
 	}
 
 	return settings
@@ -44,11 +36,94 @@ function asignar_valores(){
 
 
 function matrix(m, n) {
-  return Array.from({
-    // generate array of length m
-    length: m
+  return Array.from({ // generate array of length m
+  	length: m
     // inside map function generate array of size n
-    // and fill it with `0`
-  }, () => new Array(n).fill(2));
-};
+}, () => new Array(n).fill(2))
+}
 
+function matrixRandom(m, n) {
+	return Array.from({
+		length: m
+	}, () => new Array(n).fill().map(() => Math.round(Math.random())))
+}
+
+function ones(m, n) {
+	return Array.from({
+		length: m
+	}, () => new Array(n).fill(1))
+}
+
+
+function createNet(settings){
+	let neuralNet = new Array
+	for (i=0; i<settings.netStruct.length; i++){ 
+		neuralNet[i] = {
+			"bias" : matrix(settings.netStruct[i], 1).fill(1),
+			"synapticWeights": matrixRandom(settings.netStruct[i+1], settings.netStruct[i]),
+			"errors": [],
+			"outputs" : [],
+		}
+	}
+	for (i=1; i<settings.netStruct.length; i++){
+		if (neuralNet[i] == settings.netStruct.length){
+			neuralNet[i].errors = 1
+		}
+		else {
+			neuralNet[i].errors  = ones(settings.netStruct[i], settings.netStruct[i+1])
+		}
+	}
+	return neuralNet
+
+}
+
+
+function forwardPropagation(settings, neuralNet, j) {
+
+
+
+
+	neuralNet[0].outputs = settings.trainingInput[j]
+
+	for (i = 1; i< settings.netStruct.length; i++){
+    neuralNet[i].outputs=dotproduct(neuralNet[i-1].synapticWeights,neuralNet[i-1].outputs);  // crea una estructura de tamaño n-1 con matrices de tamaños i, i+1
+   // neuralNet[i].outputs=logsig(neuralNet[i].outputs); //haya sigmoide de cada salida de la neurona
+  
+  }
+
+	return neuralNet;
+}
+
+function logsig(n){
+	return (1 / (1 + Math.exp(-n)));
+}
+
+
+
+
+
+var f = (a, b) => [].concat(...a.map(a => b.map(b => [].concat(a, b))));
+var cartesian = (a, b, ...c) => b ? cartesian(f(a, b), ...c) : a;
+
+
+mmultiply = function(a,b) {
+	return a.map(function(x,i) {
+		return transpose(b).map(function(y,k) {
+			return dotproduct(x, y)
+		});
+	});
+}
+
+function dotproduct (a,b) {
+	return a.map(function(x,i) {
+		return a[i] * b[i];
+	}).reduce(function(m,n) { return m + n; });
+}
+
+transpose = function(a) {
+	return a[0].map(function(x,i) {
+		return a.map(function(y,k) {
+			return y[i];
+		})
+	});
+}
